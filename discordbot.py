a97173b97e5944f390766930ad95823a6df9a6da
rk7255 <60169139+rk7255@users.noreply.github.com>
2022-03-22 21:24:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,16 +27,14 @@ async def slot(ctx):
     await ctx.send("%s%s\n%s%s" % (A, B, C, D))
     
 @bot.command()
-async def slot_test(ctx):
+async def test(ctx):
     slot_list = ["<:n1:952222321538654339>", "<:n2:952222338957606923>", "<:n3:952222351586631740>", "<:n4:952222370154811452>"]
     A = random.choice(slot_list)
     B = random.choice(slot_list)
     C = random.choice(slot_list)
     D = random.choice(slot_list)
-    
-   result = "%s%s\n%s%s" % (A, B, C, D)
-    
-    await ctx.send(result)
+    #await ctx.send("%s%s\n%s%s" % (A, B, C, D))
+    await ctx.send("<:n1:952222321538654339><:n2:952222338957606923>/n<:n3:952222351586631740><:n4:952222370154811452>)
     
 
 
