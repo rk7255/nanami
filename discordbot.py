09d8abe8d1a84d0995f055d931281305ea6db4a7
rk7255 <60169139+rk7255@users.noreply.github.com>
2022-03-22 22:08:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,6 +30,22 @@ async def slot(ctx):
     await ctx.send(s)
     if s == correct :
             await ctx.send("Congrats!!!")
+            
+@bot.command()
+async def test(ctx):
+    slot_list = ["<:n1:952222321538654339>", "<:n2:952222338957606923>", "<:n3:952222351586631740>", "<:n4:952222370154811452>"]
+    list_shuffle = random.sample(slot_list, 4)
+    A = list_shuffle[0]
+    B = list_shuffle[1]
+    C = list_shuffle[2]
+    D = list_shuffle[3]
+    
+    s = A + B + '\n' + C + D
+    correct = "<:n1:952222321538654339>" + "<:n2:952222338957606923>" + '\n' + "<:n3:952222351586631740>" + "<:n4:952222370154811452>"
+    await ctx.send(s)
+    if s == correct :
+            await ctx.send("Congrats!!!")
+            
     
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
