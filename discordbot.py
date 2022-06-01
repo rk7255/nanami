12f218bd70fa611cca88980f66e96f3e325473b0
rk7255 <60169139+rk7255@users.noreply.github.com>
2022-06-01 23:24:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,6 +33,23 @@ async def slot(ctx):
     if s == correct :
             await ctx.send("Congrats!!!")
             
+@bot.command()
+async def baka(ctx):
+    slot_list = ["<:nr1:980975270863446016>", "<:nr2:980975382884933672>", "<:nr3:980975433992523847>", "<:nr4:980975462018850866>"]
+    list_shuffle = random.sample(slot_list, 4)
+    A = list_shuffle[0]
+    B = list_shuffle[1]
+    C = list_shuffle[2]
+    D = list_shuffle[3]
+    
+    s = A + B + '\n' + C + D
+    correct = "<:nr1:980975270863446016>" + "<:nr2:980975382884933672>" + '\n' + "<:nr3:980975433992523847>" + "<:nr4:980975462018850866>"
+    await ctx.send(s)
+    if s == correct :
+            await ctx.send("Congrats!!!")
+       
+      
+            
     
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
