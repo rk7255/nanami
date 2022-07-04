9a56568b06159b3fa1887c6a30fc9ac9a8062163
rk7255 <60169139+rk7255@users.noreply.github.com>
2022-07-04 21:45:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,17 +5,17 @@ import random
 
 bot = commands.Bot(command_prefix='/')
    
-'''
 @bot.event
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+   orig_error = getattr(error, "original", error)
+   error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-'''
+   client = discord.Client()
+   send_ch = client.get_channel(983969881336729630)
+
+   await send_ch.send(error_msg)
+
+
 
 @bot.command()
 async def slot(ctx):
